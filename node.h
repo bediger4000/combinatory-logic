4cc03dfc9453e79162d5977eab70689f7eb7d60c
bediger <>
2006-08-03 19:55:19 +0000

    Initial import

---
+++ node.h
@@ -0,0 +1,15 @@
+enum nodeType { UNTYPED, UNALLOCATED, APPLICATION, COMBINATOR };
+
+struct node {
+	enum nodeType typ;
+	int refcnt;
+	const char *name;
+	struct node *left;
+	struct node *right;
+};
+
+struct node *new_application(struct node *left_child, struct node *right_child);
+struct node *new_combinator(const char *name);
+
+void print_tree(struct node *root);
+void free_all_nodes(void);
