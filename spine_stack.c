90ff2b686149e4c62e95dcd8e3a32fc47ebaf47b
bediger <>
2006-11-11 15:28:50 +0000

    1. Add memory usage summary output. 2. Instead of mere malloc/free of stacks, keep a free list of previously-malloced structs spine_stack, and give them out when possible, instead of mallocing a new struct.

--- spine_stack.c
+++ spine_stack.c
@@ -1,17 +1,38 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <spine_stack.h>
+
+static struct spine_stack *spine_stack_free_list = NULL;
+
+static int stack_malloc_cnt = 0;
+static int stack_reused_cnt = 0;
+static int new_stack_cnt;
 
 struct spine_stack *
 new_spine_stack(int sz)
 {
 	struct spine_stack *r;
 
-	r = malloc(sizeof(*r));
+	++new_stack_cnt;
 
-	r->stack = malloc(sz * sizeof(r->stack[0]));
-	r->top   = 0;
-	r->size = sz;
+	if (spine_stack_free_list)
+	{
+		/* dual use of prev field: linked list of "free" spine stacks,
+		 * and FIFO stack of actually in-use spine stacks. */
+		r = spine_stack_free_list;
+		spine_stack_free_list = r->prev;
+		r->prev = NULL;
+		++stack_reused_cnt;
+	} else {
+		r = malloc(sizeof(*r));
+		r->stack = malloc(sz * sizeof(r->stack[0]));
+		r->top   = 0;
+		r->maxdepth = 0;
+		r->size = sz;
+		r->prev = NULL;
+		++stack_malloc_cnt;
+	}
 
 	return r;
 }
@@ -19,15 +40,19 @@ new_spine_stack(int sz)
 void
 delete_spine_stack(struct spine_stack *ss)
 {
-	free(ss->stack);
-	ss->stack = NULL;
-	free(ss);
+	/* dual use of prev field: linked list of "free" spine stacks,
+	 * and FIFO stack of actually in-use spine stacks. */
+	ss->prev = spine_stack_free_list;
+	spine_stack_free_list = ss;
 }
 
 void
 push_spine_stack(struct spine_stack **ss)
 {
-	struct spine_stack *p = new_spine_stack(*ss?(*ss)->size: 256);
+	/* dual use of prev field: linked list of "free" spine stacks,
+	 * and FIFO stack of actually in-use spine stacks.
+ 	 * This constitutes the FIFO stack of in-use spine stacks usage. */
+	struct spine_stack *p = new_spine_stack(*ss?(*ss)->size: 1024);
 	p->prev = *ss;
 	*ss = p;
 }
@@ -35,7 +60,42 @@ push_spine_stack(struct spine_stack **ss)
 void
 pop_spine_stack(struct spine_stack **ss)
 {
+	/* FIFO-stack-of-in-use-stacks use of prev field */
 	struct spine_stack *tmp = (*ss)->prev;
 	delete_spine_stack(*ss);
 	*ss = tmp;
+}
+
+void
+free_all_spine_stacks(int memory_info_flag)
+{
+	int maxdepth = 0;
+	int depth_sum = 0;
+	int stack_freed_cnt = 0;
+	while (spine_stack_free_list)
+	{
+		struct spine_stack *tmp = spine_stack_free_list->prev;
+		if (spine_stack_free_list->maxdepth > maxdepth)
+			maxdepth = spine_stack_free_list->maxdepth;
+		depth_sum += spine_stack_free_list->maxdepth;
+		free(spine_stack_free_list->stack);
+		spine_stack_free_list->stack = NULL;
+		free(spine_stack_free_list);
+		spine_stack_free_list = tmp;
+		++stack_freed_cnt;
+	}
+
+	if (stack_freed_cnt != stack_malloc_cnt)
+		fprintf(stderr, "Allocated %d spine stacks, freed %d\n",
+			stack_malloc_cnt, stack_freed_cnt);
+
+	if (memory_info_flag)
+	{
+		float mean_depth = stack_freed_cnt? (float)depth_sum/(float)stack_freed_cnt: 0.0;
+		fprintf(stderr, "Gave out %d spine stacks\n", new_stack_cnt);
+		fprintf(stderr, "Allocated %d spine stacks\n", stack_malloc_cnt);
+		fprintf(stderr, "Reused %d spine stacks\n", stack_reused_cnt);
+		fprintf(stderr, "Maximum spine stack depth: %d\n", maxdepth);
+		fprintf(stderr, "Mean spine stack depth: %.02f\n", mean_depth);
+	}
 }
--- spine_stack.h
+++ spine_stack.h
@@ -1,6 +1,7 @@
 struct spine_stack {
 	struct node **stack;
 	int top;
+	int maxdepth;
 	int size;
 	struct spine_stack *prev;
 };
@@ -9,6 +10,7 @@ struct spine_stack *new_spine_stack(int sz);
 void delete_spine_stack(struct spine_stack *ss);
 void push_spine_stack(struct spine_stack **ss);
 void pop_spine_stack(struct spine_stack **ss);
+void free_all_spine_stacks(int memory_info_flag);
 
 #define TOPNODE(ss) ((ss)->stack[(ss)->top - 1])
 #define PARENTNODE(ss, N) ((ss)->stack[((ss)->top)-1-N])
